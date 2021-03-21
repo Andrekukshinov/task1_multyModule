55d8a57686b3ec025d0f8991bcdac4f3fb01e681
Андрей <andrekukshinov@gmail.com>
2021-03-21 17:07:59 +0300

    get negative and strings method added

--- api/src/main/java/com/epam/application/App.java
+++ api/src/main/java/com/epam/application/App.java
@@ -4,19 +4,23 @@ import com.epam.utils.Utils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.List;
+
 public class App {
     private static final Logger LOGGER = LogManager.getLogger(App.class);
     private static final String SEVENTY_NINE = "79";
     private static final String TWELVE = "12";
-    private static final String MESSAGE = "not all values are positive";
+    private static final String MESSAGE = "not positive values are: ";
 
     public static void main(String[] args) {
         process();
     }
-    public static void process() {
-        boolean allPositiveNumbers = Utils.isAllPositiveNumbers(TWELVE, SEVENTY_NINE);
+    private static void process() {
+        String[] values = {TWELVE, SEVENTY_NINE};
+        boolean allPositiveNumbers = Utils.isAllPositiveNumbers(values);
         if(!allPositiveNumbers){
-            LOGGER.info(MESSAGE);
+            List<String> negativeAndStrings = Utils.getNegativeAndStrings(values);
+            LOGGER.info(MESSAGE + negativeAndStrings);
         }
     }
 }
--- core/src/main/java/com/epam/utils/Utils.java
+++ core/src/main/java/com/epam/utils/Utils.java
@@ -3,9 +3,15 @@ package com.epam.utils;
 import com.epam.utils.utils.StringUtils;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(String... str) {
         return Arrays.stream(str).allMatch(StringUtils::isPositiveNumber);
+    }
+
+    public static List<String> getNegativeAndStrings(String... str) {
+       return Arrays.stream(str).filter(number-> !StringUtils.isPositiveNumber(number)).collect(Collectors.toList());
     }
 }
