40cf66f890c84ae97e7bf83517c01a8a928843c4
Андрей <andrekukshinov@gmail.com>
2021-03-21 13:24:41 +0300

    log massage and log info refactored

--- api/src/main/java/com/epam/application/App.java
+++ api/src/main/java/com/epam/application/App.java
@@ -8,15 +8,9 @@ public class App {
     private static final Logger LOGGER = LogManager.getLogger(App.class);
     private static final String SEVENTY_NINE = "79";
     private static final String TWELVE = "12";
-    private static final String MESSAGE = "not all values are positive";
 
     public static void main(String[] args) {
-        process();
-    }
-    public static void process() {
         boolean allPositiveNumbers = Utils.isAllPositiveNumbers(TWELVE, SEVENTY_NINE);
-        if(!allPositiveNumbers){
-            LOGGER.info(MESSAGE);
-        }
+        LOGGER.info(allPositiveNumbers);
     }
 }
