33a203a93f4fec3939f830cb9b4b3ffec30350bf
Андрей <andrekukshinov@gmail.com>
2021-03-22 23:44:21 +0300

    pool request conversation resolved

--- core/src/main/java/com/epam/utils/Utils.java
+++ core/src/main/java/com/epam/utils/Utils.java
@@ -4,34 +4,25 @@ import com.epam.utils.utils.StringUtils;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.commons.collections4.CollectionUtils;
-//import org.apache.commons.lang3.StringUtils;
 
 
 
 public class Utils {
 
-    private static final String POSITIVE_NUMBERS = "^([1-9])[0-9]*$";
-    private static final Pattern PATTERN = Pattern.compile(POSITIVE_NUMBERS);
+    private static final String POSITIVE_NUMBERS_REGEX = "^([1-9])[0-9]*$";
+    private static final Pattern PATTERN = Pattern.compile(POSITIVE_NUMBERS_REGEX);
 
     public static boolean isAllPositiveNumbers(String... str) {
        return Arrays.stream(str).allMatch(StringUtils::isPositiveNumber);
     }
-//
-//    public static List<String> getNegativeAndStrings(String... str) {
-//        return (List<String>)CollectionUtils.selectRejected(
-//                List.of(str),
-//                (value)-> PATTERN.matcher(value).matches()
-//        );
-//    }
 
     public static List<String> getNegativeAndStrings(String... str) {
         return (List<String>)CollectionUtils.selectRejected(
                 List.of(str),
-                StringUtils::isPositiveNumber
+                (value)-> PATTERN.matcher(value).matches()
         );
     }
 }
--- core/src/test/java/com/epam/utils/UtilsTest.java
+++ core/src/test/java/com/epam/utils/UtilsTest.java
@@ -3,21 +3,15 @@ package com.epam.utils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.Collection;
-import java.util.Date;
 import java.util.List;
-import java.util.ListIterator;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class UtilsTest {
-    private static final String[] START = {"12", "", "103", "0", "AVD"};
+    private static final String[] INPUT_NUMBERS = {"12", "", "103", "0", "AVD"};
     private static final String[] EXPECTED = {"", "0", "AVD"};
-
 
     @Test
     void getNegativeAndStrings() {
-        List<String> actual = Utils.getNegativeAndStrings(START);
+        List<String> actual = Utils.getNegativeAndStrings(INPUT_NUMBERS);
         Assertions.assertEquals(List.of(EXPECTED), actual);
     }
 }
