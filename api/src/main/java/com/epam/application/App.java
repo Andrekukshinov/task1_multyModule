42fef4115cb3c625a867170806830bb9ebf8a634
Андрей <andrekukshinov@gmail.com>
2021-03-22 14:36:33 +0300

    log message updated

--- api/src/main/java/com/epam/application/App.java
+++ api/src/main/java/com/epam/application/App.java
@@ -4,14 +4,13 @@ import com.epam.utils.Utils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.Collection;
 import java.util.List;
 
 public class App {
     private static final Logger LOGGER = LogManager.getLogger(App.class);
     private static final String SEVENTY_NINE = "79";
     private static final String TWELVE = "12";
-    private static final String MESSAGE = "not positive values are: ";
+    private static final String INVALID_VALUES = "not positive values are: ";
 
     public static void main(String[] args) {
         process();
@@ -22,7 +21,7 @@ public class App {
         boolean allPositiveNumbers = Utils.isAllPositiveNumbers(values);
         if (!allPositiveNumbers) {
             List<String> negativeAndStrings = Utils.getNegativeAndStrings(values);
-            LOGGER.info(MESSAGE + negativeAndStrings);
+            LOGGER.info(INVALID_VALUES + negativeAndStrings);
         }
     }
 }
